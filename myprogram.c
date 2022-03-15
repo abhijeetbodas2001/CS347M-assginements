abbffe54945719fd2cc8c4feac720ca8f0dde8b2
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2022-03-15 19:58:51 +0530

    Add testing code for numvp()
    Use malloc to verify that pages are added when more memory is allocated

--- myprogram.c
+++ myprogram.c
@@ -2,10 +2,13 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
-
+#include "mmu.h"
 
 int main(void)
 {
+  void* i = malloc(PGSIZE*10);
+  printf(1, "%d\n", i);
+
   int virt = numvp(), phy = numpp();
   printf(2, "%d\n", virt);
   printf(2, "%d\n", phy);
